304f4b63da4101a6b4bac484424de5445eeecce5
Steven Degutis <steven@cleancoders.com>
2013-10-20 13:22:05 -0500

    type inference ftw

--- Leviathan/lexer.mm
+++ Leviathan/lexer.mm
@@ -16,7 +16,7 @@ namespace leviathan {
             std::vector<token> tokens;
             ParserError error = ParserError{ParserError::NoError};
             
-            tokens.push_back(token{token::Begin, ""});
+            tokens.push_back({token::Begin, ""});
             
             static std::string endAtomCharSet = "()[]{}, \"\r\n\t;";
             
@@ -27,57 +27,57 @@ namespace leviathan {
                 char c = raw.at(i);
                 
                 switch (c) {
-                    case '(': tokens.push_back(token{token::LParen, raw.substr(i, 1)}); break;
-                    case ')': tokens.push_back(token{token::RParen, raw.substr(i, 1)}); break;
-                    case '[': tokens.push_back(token{token::LBracket, raw.substr(i, 1)}); break;
-                    case ']': tokens.push_back(token{token::RBracket, raw.substr(i, 1)}); break;
-                    case '{': tokens.push_back(token{token::LBrace, raw.substr(i, 1)}); break;
-                    case '}': tokens.push_back(token{token::RBrace, raw.substr(i, 1)}); break;
+                    case '(': tokens.push_back({token::LParen, raw.substr(i, 1)}); break;
+                    case ')': tokens.push_back({token::RParen, raw.substr(i, 1)}); break;
+                    case '[': tokens.push_back({token::LBracket, raw.substr(i, 1)}); break;
+                    case ']': tokens.push_back({token::RBracket, raw.substr(i, 1)}); break;
+                    case '{': tokens.push_back({token::LBrace, raw.substr(i, 1)}); break;
+                    case '}': tokens.push_back({token::RBrace, raw.substr(i, 1)}); break;
                         
-                    case '\'': tokens.push_back(token{token::Quote, raw.substr(i, 1)}); break;
-                    case '`': tokens.push_back(token{token::SyntaxQuote, raw.substr(i, 1)}); break;
-                    case '^': tokens.push_back(token{token::TypeOp, raw.substr(i, 1)}); break;
+                    case '\'': tokens.push_back({token::Quote, raw.substr(i, 1)}); break;
+                    case '`': tokens.push_back({token::SyntaxQuote, raw.substr(i, 1)}); break;
+                    case '^': tokens.push_back({token::TypeOp, raw.substr(i, 1)}); break;
                         
-                    case ',': tokens.push_back(token{token::Comma, raw.substr(i, 1)}); break;
-                    case '\n': tokens.push_back(token{token::Newline, raw.substr(i, 1)}); break;
+                    case ',': tokens.push_back({token::Comma, raw.substr(i, 1)}); break;
+                    case '\n': tokens.push_back({token::Newline, raw.substr(i, 1)}); break;
                         
                     case '~': {
                         if (i + 1 < raw.length() && raw[i+1] == '@')
-                            tokens.push_back(token{token::Splice, raw.substr(i, 2)});
+                            tokens.push_back({token::Splice, raw.substr(i, 2)});
                         else
-                            tokens.push_back(token{token::Unquote, raw.substr(i, 1)});
+                            tokens.push_back({token::Unquote, raw.substr(i, 1)});
                         break;
                     }
                         
                     case ' ': {
                         size_t n = raw.find_first_not_of(" ", i);
-                        tokens.push_back(token{token::Spaces, raw.substr(i, n - i)});
+                        tokens.push_back({token::Spaces, raw.substr(i, n - i)});
                         i = n-1;
                         break;
                     }
                         
                     case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '0': {
                         size_t n = raw.find_first_of(endAtomCharSet, i);
-                        tokens.push_back(token{token::Number, raw.substr(i, n - i)});
+                        tokens.push_back({token::Number, raw.substr(i, n - i)});
                         i = n-1;
                         break;
                     }
                         
                     case ':': {
                         size_t n = raw.find_first_of(endAtomCharSet, i);
-                        tokens.push_back(token{token::Keyword, raw.substr(i, n - i)});
+                        tokens.push_back({token::Keyword, raw.substr(i, n - i)});
                         i = n-1;
                         break;
                     }
                         
                     case ';': {
                         size_t n = raw.find('\n', i);
-                        tokens.push_back(token{token::Comment, raw.substr(i, n - i)});
+                        tokens.push_back({token::Comment, raw.substr(i, n - i)});
                         i = n-1;
                         break;
                     }
                         
-                    case '\t': tokens.push_back(token{token::Spaces, std::string(2, ' ')}); break;
+                    case '\t': tokens.push_back({token::Spaces, std::string(2, ' ')}); break;
                         
                     case '"': {
                         size_t look_from = i;
@@ -91,7 +91,7 @@ namespace leviathan {
                             }
                         } while (raw[look_from - 1] == '\\');
                         
-                        tokens.push_back(token{token::String, raw.substr(i, look_from - i + 1)});
+                        tokens.push_back({token::String, raw.substr(i, look_from - i + 1)});
                         i = look_from;
                         
                         break;
@@ -102,7 +102,7 @@ namespace leviathan {
                         //       so we need to do that calculation here, and somehow store it on a token. should every token have that info? maybe its just a new TokenType.
                         
                         size_t n = raw.find_first_of(endAtomCharSet, i);
-                        tokens.push_back(token{token::Symbol, raw.substr(i, n - i)});
+                        tokens.push_back({token::Symbol, raw.substr(i, n - i)});
                         i = n-1;
                         
                         break;
@@ -112,7 +112,7 @@ namespace leviathan {
                 i++;
             }
             
-            tokens.push_back(token{token::End, ""});
+            tokens.push_back({token::End, ""});
             
             return std::make_pair(tokens, error);
         }
