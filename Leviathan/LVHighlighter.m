231dea91f8bd2399def678b1b4e0c530fba036de
Steven Degutis <steven@cleancoders.com>
2013-10-24 01:51:46 -0500

    atoms!

--- Leviathan/LVHighlighter.m
+++ Leviathan/LVHighlighter.m
@@ -51,22 +51,20 @@ static void highlight(LVElement* element, NSTextStorage* attrString, int deepnes
     else {
         LVAtom* atom = (void*)element;
         
-//        switch (atom->atomType) {
-//            case LVAtomType_Symbol: [[LVThemeManager sharedThemeManager].currentTheme.symbol highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Keyword: [[LVThemeManager sharedThemeManager].currentTheme.keyword highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_String: [[LVThemeManager sharedThemeManager].currentTheme.string highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Regex: [[LVThemeManager sharedThemeManager].currentTheme.regex highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Number: [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_True: [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: atom.token.range depth: deepness]; break; // TODO: true, false, and nil should have their own theme keys
-//            case LVAtomType_False: [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Nil: [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Comment: [[LVThemeManager sharedThemeManager].currentTheme.comment highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_TypeOp: [[LVThemeManager sharedThemeManager].currentTheme.typeop highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Quote: [[LVThemeManager sharedThemeManager].currentTheme.quote highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Unquote: [[LVThemeManager sharedThemeManager].currentTheme.unquote highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_SyntaxQuote: [[LVThemeManager sharedThemeManager].currentTheme.syntaxquote highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//            case LVAtomType_Splice: [[LVThemeManager sharedThemeManager].currentTheme.splice highlightIn: attrString range: atom.token.range depth: deepness]; break;
-//        }
+        if (atom->atomType & LVAtomType_Symbol) [[LVThemeManager sharedThemeManager].currentTheme.symbol highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Keyword) [[LVThemeManager sharedThemeManager].currentTheme.keyword highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_String) [[LVThemeManager sharedThemeManager].currentTheme.string highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Regex) [[LVThemeManager sharedThemeManager].currentTheme.regex highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Number) [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_TrueAtom) [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness]; // TODO: true, false, and nil should have their own theme keys
+        else if (atom->atomType & LVAtomType_FalseAtom) [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_NilAtom) [[LVThemeManager sharedThemeManager].currentTheme.number highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Comment) [[LVThemeManager sharedThemeManager].currentTheme.comment highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_TypeOp) [[LVThemeManager sharedThemeManager].currentTheme.typeop highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Quote) [[LVThemeManager sharedThemeManager].currentTheme.quote highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Unquote) [[LVThemeManager sharedThemeManager].currentTheme.unquote highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_SyntaxQuote) [[LVThemeManager sharedThemeManager].currentTheme.syntaxquote highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
+        else if (atom->atomType & LVAtomType_Splice) [[LVThemeManager sharedThemeManager].currentTheme.splice highlightIn: attrString range: NSMakeRange(*startPos, atom->token->val->slen) depth: deepness];
         
         *startPos += atom->token->val->slen;
     }
