d9e9fec5d84e6a5ea4f1ff2177cab9ff72fd0bb6
Steven Degutis <steven@cleancoders.com>
2013-10-28 15:56:51 -0500

    woo, find indicator!

--- Leviathan/LVEditor.m
+++ Leviathan/LVEditor.m
@@ -30,6 +30,7 @@
     size_t absPos = def.defName->token->pos;
     self.textView.selectedRange = NSMakeRange(absPos, 0);
     [self.textView scrollRangeToVisible:self.textView.selectedRange];
+    [self.textView showFindIndicatorForRange:NSMakeRange(def.defName->token->pos, def.defName->token->string->slen)];
 }
 
 - (void) startEditingFile:(LVFile*)file {
