7e2341823ba2273e29e4f83ed91e968fdbf81b86
Steven Degutis <steven@cleancoders.com>
2013-10-28 12:02:09 -0500

    EDN again for now

--- Leviathan/configs.m
+++ Leviathan/configs.m
@@ -8,104 +8,36 @@
 
 #import "configs.h"
 
-#import "parser.h"
-#import "atom.h"
+#import "BMOEDNSerialization.h"
+#import "BMOEDNKeyword.h"
 
-id LVContainerFromColl(LVColl* coll);
-
-id LVSingleFromAtom(LVAtom* atom) {
-    if (atom->atom_type & LVAtomType_Keyword) {
-        struct tagbstring kw_val;
-        bmid2tbstr(kw_val, atom->token->string, 1, atom->token->string->slen - 1);
-        return [NSString stringWithFormat:@"%s", kw_val.data];
-    }
-    else if (atom->atom_type & LVAtomType_TrueAtom) { 
-        return @YES;
-    }
-    else if (atom->atom_type & LVAtomType_FalseAtom) {
-        return @NO;
-    }
-    else if (atom->atom_type & LVAtomType_String) {
-        bstring kw_val = bmidstr(atom->token->string, 1, atom->token->string->slen - 2);
-        id s = [NSString stringWithFormat:@"%s", kw_val->data];
-        bdestroy(kw_val);
-        return s;
-    }
-    abort();
-}
-
-NSArray* LVArrayFromColl(LVColl* list) {
-    NSMutableArray* array = [NSMutableArray array];
-    
-    for (int i = 1; i < list->children_len; i++) {
-        LVElement* child = list->children[i];
-        
-        id found;
-        
-        if (child->is_atom) {
-            LVAtom* atom = (void*)child;
-            
-            if (!LVAtomIsSemantic(atom))
-                continue;
-            
-            found = LVSingleFromAtom(atom);
+static id fix(id thing) {
+    if ([thing isKindOfClass:[NSArray self]]) {
+        NSMutableArray* array = [NSMutableArray array];
+        for (id child in thing) {
+            [array addObject: fix(child)];
         }
-        else {
-            found = LVContainerFromColl((void*)child);
-        }
-        
-        [array addObject: found];
+        return array;
     }
-    
-    return array;
-}
-
-NSDictionary* LVDictionaryFromColl(LVColl* map) {
-    NSMutableDictionary* dict = [NSMutableDictionary dictionary];
-    
-    id key;
-    
-    for (int i = 1; i < map->children_len; i++) {
-        LVElement* child = map->children[i];
-        
-        id found;
-        
-        if (child->is_atom) {
-            LVAtom* atom = (void*)child;
-            
-            if (!LVAtomIsSemantic(atom))
-                continue;
-            
-            found = LVSingleFromAtom(atom);
+    if ([thing isKindOfClass:[NSDictionary self]]) {
+        NSMutableDictionary* dict = [NSMutableDictionary dictionary];
+        for (id key in thing) {
+            id val = [thing objectForKey: key];
+            [dict setObject:fix(val) forKey:fix(key)];
         }
-        else {
-            found = LVContainerFromColl((void*)child);
-        }
-        
-        if (!key) {
-            key = found;
-        }
-        else {
-            [dict setObject:found forKey:key];
-            key = nil;
-        }
+        return dict;
     }
-    
-    return dict;
-}
-
-id LVContainerFromColl(LVColl* coll) {
-    if (coll->coll_type & LVCollType_Map)
-        return LVDictionaryFromColl(coll);
-    else if (coll->coll_type & LVCollType_Vector)
-        return LVArrayFromColl(coll);
-    
-    abort();
+    else if ([thing isKindOfClass:[BMOEDNKeyword self]]) {
+        return [thing name];
+    }
+    else {
+        return thing;
+    }
 }
 
 id LVParseConfigFromString(NSString* str) {
-    LVColl* coll = LVParse([str UTF8String]);
-    NSDictionary* configs = LVContainerFromColl((void*)coll->children[1]);
-    LVCollDestroy(coll);
-    return configs;
+    NSData* data = [str dataUsingEncoding:NSUTF8StringEncoding];
+    id config = [BMOEDNSerialization ednObjectWithData:data options:0 error:NULL];
+    config = fix(config);
+    return config;
 }
---
+++ Leviathan/edn-objc/BMOEDNKeyword.m
@@ -0,0 +1,31 @@
+//
+//  BMOEDNKeyword.m
+//  edn-objc
+//
+//  Created by Ben Mosher on 8/24/13.
+//  Copyright (c) 2013 Ben Mosher. All rights reserved.
+//
+
+#import "BMOEDNKeyword.h"
+
+@implementation BMOEDNKeyword
+
++(BMOEDNKeyword *)keywordWithNamespace:(NSString *)ns name:(NSString *)name {
+    return [[BMOEDNKeyword alloc] initWithNamespace:ns name:name];
+}
+
+-(BOOL)isEqual:(id)object {
+    if (object == self) return YES;
+    if (![object isMemberOfClass:[BMOEDNKeyword class]]) return NO;
+    return [self isEqualToKeyword:(BMOEDNKeyword *)object];
+}
+
+-(BOOL)isEqualToKeyword:(BMOEDNKeyword *)object {
+    return [super isEqualToSymbol:object];
+}
+
+-(NSString *)description {
+    return [@":" stringByAppendingString:[super description]];
+}
+
+@end
