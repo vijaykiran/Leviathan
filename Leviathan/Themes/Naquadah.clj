146271fc5f3e92db9d01f40d7df52e24e2f491ec
Steven Degutis <steven@cleancoders.com>
2013-11-07 17:30:03 -0600

    themes

---
+++ Leviathan/Themes/Naquadah.clj
@@ -0,0 +1,34 @@
+;; any map that has :color can have :italic and :bold (booleans), which default to false
+
+{:cursor-color "babdb6"
+ ;; :highlight-line-color "000000"
+ :background-color "252A2B"
+ :selection-style {:background-color "5c3566"}
+ :unquote-style {:color "edd400" :bold true}
+ :syntaxquote-style {:color "edd400" :bold true}
+ :regex-style {:color "e9b96e"}
+ :symbol-style {:color "eeeeec"}
+ :typehint-style {:color "edd400" :bold true}
+ :keyword-style {:color "73d216"}
+ :number-style {:color "99bbff"}
+ :true-style {:color "99bbff" :bold true}
+ :false-style {:color "99bbff" :bold true}
+ :nil-style {:color "99bbff" :bold true}
+ :string-style {:color "ad7fa8"}
+ :def-name-style {:color "edd400" :bold true}
+ :splice-style {:color "edd400" :bold true}
+ :syntax-error-style {:color "ef2929" :bold true}
+ :rainbow-parens-styles [{:color "729fcf"}
+                         {:color "8ae234"}
+                         {:color "fce94f"}
+                         {:color "ad7fa8"}
+                         {:color "e9b96e"}
+                         {:color "fcaf3e"}
+                         {:color "3465a4"}
+                         {:color "73d216"}
+                         {:color "f57900"}
+                         {:color "75507b"}
+                         {:color "c17d11"}]
+ :def-keyord-style {:color "729fcf" :bold true}
+ :quote-style {:color "edd400" :bold true}
+ :comment-style {:color "999999"}}
---
+++ Leviathan/Themes/TomorrowNightEighties.clj
@@ -0,0 +1,36 @@
+{:cursor-color "ffffff"
+ :highlight-line-color "393939"
+ :background-color "2d2d2d"
+
+ :selection-style {:background-color "515151"}
+
+ :comment-style {:color "999999"}
+
+ :unquote-style {:color "ffcc66" :bold true}
+ :syntaxquote-style {:color "ffcc66" :bold true}
+ :splice-style {:color "ffcc66" :bold true}
+ :typehint-style {:color "ffcc66" :bold true}
+ :quote-style {:color "ffcc66" :bold true}
+
+ :number-style {:color "6699cc"}
+ :string-style {:color "f99157"}
+ :symbol-style {:color "cccccc"}
+ :keyword-style {:color "cc99cc"}
+ :regex-style {:color "f2777a"}
+
+ :true-style {:color "66cccc" :bold true}
+ :false-style {:color "66cccc" :bold true}
+ :nil-style {:color "66cccc" :bold true}
+
+ :def-name-style {:color "ffcc66" :bold true}
+ :def-keyord-style {:color "6699cc" :bold true}
+
+ :syntax-error-style {:color "f2777a" :bold true}
+
+ :rainbow-parens-styles [{:color "f2777a"}
+                         {:color "f99157"}
+                         {:color "ffcc66"}
+                         {:color "99cc99"}
+                         {:color "66cccc"}
+                         {:color "6699cc"}
+                         {:color "cc99cc"}]}
